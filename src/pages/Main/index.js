0b0176da40bd9413366d884a9aa00e0bf7b9c155
Fabio Motta <fabiomotta0311@gmail.com>
2020-05-21 05:51:19 -0300

    list prev and next page, styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,31 +1,61 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import './styles.css';
 
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     componentDidMount(){
         this.loadProducts();
     }
 
-    loadProducts = async () =>{
-        const response = await api.get('/products');
-        this.setState({ products: response.data.docs });
+    loadProducts = async (page = 1) =>{
+        const response = await api.get(`/products?page=${page}`);
+
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
     };
+
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === 1) return;
+
+        const pageNumber =  page -1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+        
+        this.loadProducts(pageNumber);
+    }
 
     render(){
         const { products } = this.state;
         return (
             <div className="product-list">
-                {products.map(product =>(
+                {products.map( product =>(
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-                        <a href="">Acessar</a>
+                        <a href="/">Acessar</a>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={this.state.page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={this.state.page === this.state.productInfo.pages} onClick={this.nextPage}>Próxima</button>
+                </div>
             </div>
         )    
     }

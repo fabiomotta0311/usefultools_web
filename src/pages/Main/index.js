7624fbdaf284ec7f1e86a733cffa122efebfb8e0
Fabio Motta <fabiomotta0311@gmail.com>
2020-05-21 04:19:03 -0300

    list_products done

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+        products: [],
+    }
 
     componentDidMount(){
         this.loadProducts();
     }
 
-    loadProducts = () =>{
-        
-    }
+    loadProducts = async () =>{
+        const response = await api.get('/products');
+        this.setState({ products: response.data.docs });
+    };
 
     render(){
-        return <h1>Hello Rocketseat</h1>;
+        const { products } = this.state;
+        return (
+            <div className="product-list">
+                {products.map(product =>(
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
+            </div>
+        )    
     }
 }
